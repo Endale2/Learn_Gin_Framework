ffe045984eb82a465a4d3c5878c43171426bea3e
Endale2 <endale406@gmail.com>
2025-10-02 13:05:14 +0300

    gin.Context.Query()

--- main.go
+++ main.go
@@ -21,6 +21,14 @@ func main() {
 		id:=ctx.Param("id")
 
 		ctx.JSON(200, gin.H{"user_id":id})
+
+	})
+
+	r.GET("/search", func(c *gin.Context){
+		name:=c.Query("name")
+		age:=c.DefaultQuery("age", "0")
+       resp:=gin.H{"name":name, "age":age}
+		c.JSON(200, resp)
 	})
 
     // start server on port 8080
