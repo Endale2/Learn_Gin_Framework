e30af69f625ee98e7a2a2c1ae46d3633710c7b5f
Endale2 <endale406@gmail.com>
2025-10-03 21:51:07 +0300

    using slice  as  a db for  post  and  get   requests

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package  main
 
 import  "github.com/gin-gonic/gin"
 
-
+var books []Book
 
 type  Book  struct {
 	Title string  `json:"title"`
@@ -24,8 +24,12 @@ func  CreateBooks(c  *gin.Context){
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-
+  books = append(books, book)
 	c.JSON(200, gin.H{"message":"Book  Received", "book":book})
+}
+
+func  GetAllBooks(  c  *gin.Context){
+   c.JSON(200, books)
 }
 
 
@@ -35,6 +39,7 @@ func  main(){
 	r:=gin.Default()
 
 	r.POST("/create", CreateBooks)
+	r.GET("/books", GetAllBooks)
 
 	r.Run(":8080")
 }
